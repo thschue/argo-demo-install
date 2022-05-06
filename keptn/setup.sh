8cfc575b58bd5e51285f3fa74360124316ce69a6
Thomas Schuetz <thomas.schuetz@dynatrace.com>
2022-05-06 09:05:42 +0200

    updated configuration
    Signed-off-by: Thomas Schuetz <thomas.schuetz@dynatrace.com>

--- keptn/setup.sh
+++ keptn/setup.sh
@@ -2,3 +2,4 @@
 
 keptn add-resource --project demo --service podtatohead --all-stages --resource=k6/test.js --resourceUri=k6/test.js
 keptn add-resource --project demo --service podtatohead --all-stages --resource=job/config.yaml --resourceUri=job/config.yaml
+keptn add-resource --project demo --service podtatohead --all-stages --resource=dynatrace/dynatrace.conf.yaml --resourceUri=dynatrace/dynatrace.conf.yaml
