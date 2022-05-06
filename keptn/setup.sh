3b5a05a84bc48992d4fdeb8c94c98806331c0868
Thomas Schuetz <thomas.schuetz@dynatrace.com>
2022-05-06 12:09:03 +0200

    updated helm chart
    Signed-off-by: Thomas Schuetz <thomas.schuetz@dynatrace.com>

--- keptn/setup.sh
+++ keptn/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+keptn add-resource --project demo --service podtatohead --all-stages --resource=slo.yaml --resourceUri=slo.yaml
 keptn add-resource --project demo --service podtatohead --all-stages --resource=k6/test.js --resourceUri=k6/test.js
 keptn add-resource --project demo --service podtatohead --all-stages --resource=job/config.yaml --resourceUri=job/config.yaml
 keptn add-resource --project demo --service podtatohead --all-stages --resource=dynatrace/dynatrace.conf.yaml --resourceUri=dynatrace/dynatrace.conf.yaml
