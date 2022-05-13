6f826b404906177e2f3e2b0585c8b6456df3d328
Thomas Schuetz <thomas.schuetz@dynatrace.com>
2022-05-13 14:24:28 +0200

    change config
    Signed-off-by: Thomas Schuetz <thomas.schuetz@dynatrace.com>

--- keptn/setup.sh
+++ keptn/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-keptn add-resource --project demo --service podtatohead --all-stages --resource=slo.yaml --resourceUri=slo.yaml
-keptn add-resource --project demo --service podtatohead --all-stages --resource=k6/test.js --resourceUri=k6/test.js
-keptn add-resource --project demo --service podtatohead --all-stages --resource=job/config.yaml --resourceUri=job/config.yaml
-keptn add-resource --project demo --service podtatohead --all-stages --resource=dynatrace/dynatrace.conf.yaml --resourceUri=dynatrace/dynatrace.conf.yaml
-keptn add-resource --project demo --service podtatohead --all-stages --resource=dynatrace/sli.yaml --resourceUri=dynatrace/sli.yaml
+keptn add-resource --project argo-demo --service podtatohead --all-stages --resource=slo.yaml --resourceUri=slo.yaml
+keptn add-resource --project argo-demo --service podtatohead --all-stages --resource=k6/test.js --resourceUri=k6/test.js
+keptn add-resource --project argo-demo --service podtatohead --all-stages --resource=job/config.yaml --resourceUri=job/config.yaml
+keptn add-resource --project argo-demo --service podtatohead --all-stages --resource=dynatrace/dynatrace.conf.yaml --resourceUri=dynatrace/dynatrace.conf.yaml
+keptn add-resource --project argo-demo --service podtatohead --all-stages --resource=dynatrace/sli.yaml --resourceUri=dynatrace/sli.yaml
